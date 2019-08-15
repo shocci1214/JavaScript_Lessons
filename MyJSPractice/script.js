57b89b0f71e8a1f87a58f42e8ed02123ba79249d
ShoheiYamamoto <shocci1214@gmail.com>
2019-08-15 11:47:23 +0900

    L11終了

--- MyJSPractice/script.js
+++ MyJSPractice/script.js
@@ -1,12 +1,20 @@
 'use strict';
 
-for(let i = 0; i < 100; i++){
+const num = 5;
+const winner = Math.floor(Math.random() * num);
+
+for(let i = 0; i < num; i++){
   const div = document.createElement('div');
   div.classList.add('box');
-  div.textContent = i;
 
   div.addEventListener('click', () => {
-    div.classList.toggle('circle');
+    if(i === winner){
+      div.textContent = 'win!';
+      div.classList.add('win');
+    }else{
+      div.textContent = 'lose!';
+      div.classList.add('lose');
+    }
   });
   document.body.appendChild(div);
 }
