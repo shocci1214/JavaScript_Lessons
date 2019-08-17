0b0468b962282fe34718e2e9cb39f999c8dc91ec
ShoheiYamamoto <shocci1214@gmail.com>
2019-08-17 21:25:24 +0900

    L18まで終了

--- JsPracticeBasic/js/main.js
+++ JsPracticeBasic/js/main.js
@@ -1,10 +1,43 @@
 'use strict';
 
-console.log(typeof 'hello');
-console.log(typeof 5);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
+for(let i = 1; i <= 10; i++){
+    if(i % 3 === 0){
+        break;
+    }
+    console.log(i);
+}
+// for(let i = 1; i <= 10; i++){
+//     if(i % 3 === 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+// for(let i = 1; i <= 10; i++){
+//     if(i === 4){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// let hp = 200;
+
+// for(let hp = 200; hp > 0; hp -= 15){
+//     console.log(`${hp} HP left!`);
+// }
+// while(hp > 0){
+//     console.log(`${hp} HP left!`)
+//     hp -= 15;
+// }
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(`${i} hello`);
+// }
+
+// console.log(typeof 'hello');
+// console.log(typeof 5);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
 
 // let price = 150;
 
