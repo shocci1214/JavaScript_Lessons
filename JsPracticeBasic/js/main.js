a4c874452c943451bdcbd863928cbe151fdb97d0
ShoheiYamamoto <shocci1214@gmail.com>
2019-08-17 16:19:53 +0900

    L4まで終了

--- JsPracticeBasic/js/main.js
+++ JsPracticeBasic/js/main.js
@@ -1,4 +1,10 @@
 'use strict';
 
-console.log('Hello world!');
-console.log('Hello world! again');
+console.log('Hello world from main js!');
+
+// console.log("it's me!");
+// console.log('it\'s me!');
+
+// console.log('Hello\n world\t from main js!');
+
+console.log('Hello world'  + 'from main js!');
