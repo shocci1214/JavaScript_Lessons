273d49f9c79c63c4d975bde857015b3e6d81e6db
ShoheiYamamoto <shocci1214@gmail.com>
2019-08-17 22:52:10 +0900

    L3まで終了

--- JsPracticeObject/js/main.js
+++ JsPracticeObject/js/main.js
@@ -1,9 +1,15 @@
-'use strict'
+'use strict';
+
 {
-    const scores = [80, 90, 40];
-    console.log(scores[1]);
-    scores[2] = 100;
-    console.log(scores)
-    //   scores = 1;
-    console.log(scores.length);
+    const player = {
+        name: 'shocci', 
+        score: 52
+    };
+    console.log(player);
+    // const scores = [80, 90, 40];
+    // console.log(scores[1]);
+    // scores[2] = 100;
+    // console.log(scores)
+    // //   scores = 1;
+    // console.log(scores.length);
 }
