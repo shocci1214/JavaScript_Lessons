18ee2ae3498396ca2f5dd5b4b51604f07585fc8d
ShoheiYamamoto <shocci1214@gmail.com>
2019-08-17 16:25:46 +0900

    L7まで終了

--- JsPracticeBasic/js/main.js
+++ JsPracticeBasic/js/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
-console.log('Hello world from main js!');
+const price = 150;
+
+console.log(price * 140);
+console.log(price * 160);
+
+// console.log(10 + 3);
+// console.log(10 - 3);
+// console.log(10 * 3);
+// console.log(10 / 3);
+// console.log(10 % 3);
+// console.log(10 ** 3);
+
+// console.log('Hello world from main js!');
 
 // console.log("it's me!");
 // console.log('it\'s me!');
 
 // console.log('Hello\n world\t from main js!');
 
-console.log('Hello world'  + 'from main js!');
+// console.log('Hello world'  + 'from main js!');
