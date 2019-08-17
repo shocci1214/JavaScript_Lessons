1f7ec38d10f48caa0ecbb3a1e0d558bac4265022
ShoheiYamamoto <shocci1214@gmail.com>
2019-08-17 23:03:05 +0900

    L6まで終了

--- JsPracticeObject/js/main.js
+++ JsPracticeObject/js/main.js
@@ -1,20 +1,46 @@
 'use strict';
 
 {
-    const player = {
-        name: 'shocci', 
-        score: 52
-    };
-    console.log(player.name);
-    console.log(player['name']);
-    player.score = 100;
-    console.log(player);
 
-    player.email = 'shocci1214@gmail.com';
-    console.log(player);
+    //クラス
+    class Player{
+        constructor(name, score){
+          this.name = name;
+          this.score = score;
+        }
 
-    delete player.score;
-    console.log(player);
+        showInfo(){
+            console.log(`name: ${this.name} score: ${this.score}`);
+        }
+        
+        static showVersion(){
+            console.log('Player class ver 1.0');
+        }
+    }
+
+    const shocci = new Player('shocci', 30);
+    const takagi = new Player('takagi', 50);
+    
+    console.log(shocci.name);
+    shocci.showInfo();
+    takagi.showInfo();
+
+    Player.showVersion();
+
+    // const player = {
+    //     name: 'shocci', 
+    //     score: 52
+    // };
+    // console.log(player.name);
+    // console.log(player['name']);
+    // player.score = 100;
+    // console.log(player);
+
+    // player.email = 'shocci1214@gmail.com';
+    // console.log(player);
+
+    // delete player.score;
+    // console.log(player);
     // const scores = [80, 90, 40];
     // console.log(scores[1]);
     // scores[2] = 100;
